25a24915517dde0536ef405f7031be2e1b6edac9
kalvinosoro <kalvinosoro@gmail.com>
2023-09-09 20:01:24 +0300

    updates

--- src/main/java/com/app/todo/models/TodoList.java
+++ src/main/java/com/app/todo/models/TodoList.java
@@ -1,11 +1,23 @@
 package com.app.todo.models;
 
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 
 @Data
 @Getter
 @Setter
+@Entity
+@Table(name = "items")
 public class TodoList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    private String title;
+    @NonNull
+    private String description;
 }
